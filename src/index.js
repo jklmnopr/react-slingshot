1928fc27101fecc9e1e6d5cbe7171a100ce21e3c
Dovydas Ceilutka <dovydas@troublemaker.xyz>
2016-12-30 08:21:04 +0200

    Add React Bootstrap

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-// import { syncHistoryWithStore } from 'react-router-redux';
-// TODO: fix router
 import Routes from './Routes';
 import configureStore from './store/configureStore';
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
@@ -12,9 +10,6 @@ import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS fil
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 
 const store = configureStore();
-
-// Create an enhanced history that syncs navigation events with the store
-// const history = syncHistoryWithStore(browserHistory, store);
 
 render(
   <Provider store={store}>
